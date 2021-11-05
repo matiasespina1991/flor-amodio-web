71ee523467a641b9dfe741387bcee90bd494a5d2
Matias Espina <matiasespina1991@gmail.com>
2021-11-05 05:48:50 +0100

    head added with favicon and title

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,18 @@ import Header from '../layouts/Header'
 import TopNavbar from '../layouts/TopNavbar'
 import LeftNavbar from '../layouts/LeftNavbar';
 import MainContent from '../layouts/MainContent'
+import Head from 'next/head'
 
 
 function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        <title>AMODIOFLORI art+design</title>
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Header/>
       <div className="body-wrapper">
         <LeftNavbar />
