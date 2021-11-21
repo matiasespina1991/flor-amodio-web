987046204853c370835ef97c8013ec40d8e6837c
Matias Espina <matiasespina1991@gmail.com>
2021-11-21 04:26:39 +0100

    SEO for twitter added

--- pages/_app.js
+++ pages/_app.js
@@ -36,6 +36,11 @@ function MyApp({ Component, pageProps }) {
         <meta property="og:locale" content="en_GB" />
         <meta property="og:site_name" content="Amodioflorencia.com"/>
         <meta property="og:image" content="https://cms.amodioflorencia.com/wp-content/uploads/2021/11/og-image_thumbnail.jpg" itemProp="image" />
+        <meta property="twitter:title" content="AMODIOFLORI / art+design"/>
+        <meta property="twitter:url" content="https://www.amodioflorencia.com/"/>
+        <meta property="twitter:locale" content="en_GB" />
+        <meta property="twitter:site_name" content="Amodioflorencia.com"/>
+        <meta property="twitter:image" content="https://cms.amodioflorencia.com/wp-content/uploads/2021/11/og-image_thumbnail.jpg" itemProp="image" />
       </Head>
       {DOMloading ?
       <div></div>
