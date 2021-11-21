c340df8206ccee87fb2caaba71dc32b1f49c937e
Matias Espina <matiasespina1991@gmail.com>
2021-11-21 04:35:04 +0100

    SEO for twitter fixed

--- pages/_app.js
+++ pages/_app.js
@@ -37,11 +37,10 @@ function MyApp({ Component, pageProps }) {
         <meta property="og:site_name" content="Amodioflorencia.com"/>
         <meta property="og:image" content="https://cms.amodioflorencia.com/wp-content/uploads/2021/11/og-image_thumbnail.jpg" itemProp="image" />
         <meta name="twitter:card" content="summary" />
-        <meta property="twitter:title" content="AMODIOFLORI / art+design"/>
-        <meta property="twitter:url" content="https://www.amodioflorencia.com/"/>
-        <meta property="twitter:locale" content="en_GB" />
-        <meta property="twitter:site_name" content="Amodioflorencia.com"/>
-        <meta property="twitter:image" content="https://cms.amodioflorencia.com/wp-content/uploads/2021/11/og-image_thumbnail.jpg" itemProp="image" />
+        <meta name="twitter:title" content="AMODIOFLORI / art+design"/>
+        <meta name="twitter:site" content="@amodioflori"/>
+        <meta name="twitter:description" content="Florencia Amodio is an artist & graphic designer based in Buenos Aires, Argentina."/>
+        <meta name="twitter:image" content="https://cms.amodioflorencia.com/wp-content/uploads/2021/11/og-image_thumbnail.jpg" itemProp="image" />
       </Head>
       {DOMloading ?
       <div></div>
